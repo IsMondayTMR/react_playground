0358d0f123dc4156c8e5612dbf41d7ba28f2dce3
IsMondayTMR <47375192+IsMondayTMR@users.noreply.github.com>
2020-09-16 15:08:50 -0700

    memegenerator

---
+++ meme_generator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Meme_module from './Meme_module'
+import Header from './Header'
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Meme_module />
+
+    </div>
+  );
+}
+
+export default App;
