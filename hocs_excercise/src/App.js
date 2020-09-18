31e6b823b4f3b593c09f33bcf1d49e27b7fb6d12
IsMondayTMR <47375192+IsMondayTMR@users.noreply.github.com>
2020-09-18 16:21:45 -0700

    higher order components prac

---
+++ hocs_excercise/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+            <Menu something="whatever" />
+            <hr />
+            <Favorite />
+        </div>
+  );
+}
+
+export default App;
---
+++ hocs_excercise/src/Favorite.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+    function Menu(props){
+        return(
+            <div className = "Menu_container">
+                
+            </div>
+        )
+    }
+
+export default Menu
